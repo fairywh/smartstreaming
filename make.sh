de31b838b1aa5ce3c5ef1e856cff6138dc98af2d
rainwu <rainwu@tencent.com>
2022-09-04 11:45:55 +0800

    fix

--- make.sh
+++ make.sh
@@ -4,6 +4,8 @@ PROJECT_DIR="."
 THIRDPARTY_DIR="${PROJECT_DIR}/3rdparty"
 cd ${THIRDPARTY_DIR}/spdlog/ && cmake ./ && make -j8
 cd ../../
+cd ${THIRDPARTY_DIR}/x264/ && make
+cd ../../
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/
 cd ${THIRDPARTY_DIR}/FFmpeg/ && ./configure --enable-shared --enable-libx264 --enable-gpl --enable-pthreads --extra-cflags="-I/usr/local/include" --extra-ldflags="-L/usr/local/lib/" && make -j8
 cd ../../
